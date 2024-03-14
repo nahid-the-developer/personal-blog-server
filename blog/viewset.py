dcd7777a7c6937b5d6e177b4addb904f0f620ecb
nahid-the-developer <nahidcse.pub@gmail.com>
2024-03-15 05:15:08 +0600

    add pagination on server side

--- blog/viewset.py
+++ blog/viewset.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny, IsAuthenticated
 
+from utils.pagination import StandardResultsSetPagination
 from .models import BlogPost
 from .serializers import BlogPostSerializer
 
@@ -8,6 +9,7 @@ from .serializers import BlogPostSerializer
 class BlogPostViewSet(viewsets.ModelViewSet):
     queryset = BlogPost.objects.all().order_by('-id')
     serializer_class = BlogPostSerializer
+    pagination_class = StandardResultsSetPagination
 
     def get_permissions(self):
         if self.action in ['list', 'retrieve']:
